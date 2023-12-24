2fff44e7989161a91333d735f9f54d9c7a4b8d3e
nemawat.rakesh <nemawat.rakesh@ringcentral.com>
2023-12-24 13:38:22 +0530

    [3] Added Themes

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -12,10 +12,18 @@ export const Logo = () => {
     return (
         <div className="hidden md:flex items-center gap-x-2">
             <Image 
+                src="/logo_dark.png"
+                height={40}
+                width={40}
+                alt="Logo"
+                className="dark:hidden"
+            />
+            <Image 
                 src="/logo.png"
                 height={40}
                 width={40}
                 alt="Logo"
+                className="hidden dark:block"
             />
             <p className={cn("font-semibold", font.className)}>
                 Notion
