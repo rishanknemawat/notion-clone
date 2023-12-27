8dadca61fc32ebbeb29492eac983eaef07b2c871
nemawat.rakesh <nemawat.rakesh@ringcentral.com>
2023-12-27 21:51:43 +0530

    Add coverImage functionalities

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Cover } from "@/components/cover";
 import Toolbar from "@/components/toolbar";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -29,14 +30,14 @@ const DocumentIdPage = ({
     if (document === null) {
         return (
             <div>
-                Not found
+                Not Found
             </div>
         );
     }
 
     return ( 
         <div className="pb-40">
-            <div className="h-[35vh]" />
+            <Cover url={document.coverImage} />
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
             </div>
---
+++ components/cover.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface CoverProps {
+    url?: string; 
+    preview?: string;
+};
+
+export const Cover = ({
+    url,
+    preview,
+}: CoverProps) => {
+    const params = useParams();
+    const coverImage = useCoverImage();
+    const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+    const onRemove = () => {
+        removeCoverImage({ id: params.documentId as Id<"documents"> });
+    }
+
+    return (
+        <div className={cn(
+            "relative w-full h-[35vh] group",
+            !url && "h-[12vh]",
+            url && "bg-muted"
+        )}>
+            {!!url && (
+                <Image
+                    src={url}
+                    fill
+                    className="object-cover"
+                    alt="Cover"
+                />
+            )}
+            {url && !preview && (
+                <div className="opacity-0 group-hover:opacity-100 absolute
+                    bottom-5 right-5 flex items-center gap-x-2">
+                        <Button
+                            className="text-muted-foreground text-xs"
+                            variant={"outline"}
+                            size={"sm"}
+                            onClick={coverImage.onOpen}
+                        > 
+                            <ImageIcon className="h-4 w-4 mr-2" />
+                            Change Cover
+                        </Button>
+                        <Button
+                            className="text-muted-foreground text-xs"
+                            variant={"outline"}
+                            size={"sm"}
+                            onClick={onRemove}
+                        > 
+                            <X className="h-4 w-4 mr-2" />
+                            Remove
+                        </Button>
+                </div>
+            )}
+        </div>
+    );
+};
