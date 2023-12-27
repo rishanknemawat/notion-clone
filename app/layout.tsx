1b207f99dd31720f7e6fcbe212945dd096f49159
nemawat.rakesh <nemawat.rakesh@ringcentral.com>
2023-12-27 21:25:40 +0530

    Add File Upload functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ConvexClientProvider } from '@/components/providers/concex.provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
 
 import './globals.css'
+import { EdgeStoreProvider } from '@/lib/edgestore'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,17 +39,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
-            enableSystem
-            disableTransitionOnChange
-            storageKey='notion-clone-1'
-          >
-            <Toaster position='bottom-center'/>
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='system'
+              enableSystem
+              disableTransitionOnChange
+              storageKey='notion-clone-1'
+            >
+              <Toaster position='bottom-center'/>
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,12 +1,49 @@
 "use client";
 
+import { useState } from "react";
+import { useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 import { useCoverImage } from "@/hooks/use-cover-image";
+
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
-
-
+import { SingleImageDropzone } from "../single-image-dropzone";
+import { useParams } from "next/navigation";
 
 export const CoverImageModal = () => {
+    const params = useParams();
     const coverImage = useCoverImage();
+    const update = useMutation(api.documents.update);
+    const { edgestore } = useEdgeStore();
+    
+    const [ file, setFile ] = useState<File>();
+    const [ isSubmitting, setIsSubmitting ] = useState(false);
+
+    const onChange = async (file?: File) => {
+        if (file) {
+            setIsSubmitting(true);
+            setFile(file);
+
+            const res = await edgestore.publicFiles.upload({
+                file
+            });
+
+            await update({
+                id: params.documentId as Id<"documents">,
+                coverImage: res.url, 
+            });
+
+            onClose();
+        };
+    };
+
+    const onClose = () => {
+        setFile(undefined);
+        setIsSubmitting(false);
+        coverImage.onClose();
+    }
 
     return (
         <Dialog
@@ -19,9 +56,12 @@ export const CoverImageModal = () => {
                         Cover Image
                     </h2>
                 </DialogHeader>
-                <div>
-                    TODO: Upload Image 
-                </div>
+                <SingleImageDropzone
+                    className="w-full outline-none"
+                    disabled={isSubmitting}
+                    value={file}
+                    onChange={onChange}
+                />
             </DialogContent>
         </Dialog>
     );
