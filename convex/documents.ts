1f1aaa09d5de611409ff0deab4fa59937be7a547
nemawat.rakesh <nemawat.rakesh@ringcentral.com>
2023-12-28 20:00:51 +0530

    Added Editor

--- convex/documents.ts
+++ convex/documents.ts
@@ -257,7 +257,7 @@ export const update = mutation({
   args: {
     id: v.id("documents"),
     title: v.optional(v.string()),
-    contetnt: v.optional(v.string()),
+    content: v.optional(v.string()),
     coverImage: v.optional(v.string()),
     icon: v.optional(v.string()),
     isPublished: v.optional(v.boolean()),
